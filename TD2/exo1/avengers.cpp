89a0087bb6f5c7539e933e2049294ba1cde1850a
meelf <32335197+meelf@users.noreply.github.com>
2017-09-27 11:16:42 -0400

    TD2 exo1 sur VS

---
+++ TD2/exo1/avengers.cpp
@@ -0,0 +1,22 @@
+/////////////////////////////////////////
+// Nom du fichier: avengers.cpp
+// Auteur: Mehdi El Fad 1783236
+// Date: 25 - 09 -2017
+// Descrpition: Encourage un Avengers apres lui avoir demandé son nom et son pouvoir
+//////////////////////////////////////////
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+	string nom, pouvoir;
+	cout << "Entrer votre nom : " << endl;
+	cin >> nom;
+	cout << "Entrer votre pouvoir : " << endl;
+	cin >> pouvoir;
+	cout << nom << " est le meilleur " << pouvoir << " au monde entier ! " << endl;
+
+
+}
